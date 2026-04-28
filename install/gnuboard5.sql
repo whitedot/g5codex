2e7ff5978a21a6faadec6d40fe03d795a0fcadd9
whitedot <whitedot@naver.com>
2026-04-28 18:39:55 +0900

    복원: 설치 파일 추가

---
+++ install/gnuboard5.sql
@@ -0,0 +1,157 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g5_auth`
+--
+
+DROP TABLE IF EXISTS `g5_auth`;
+CREATE TABLE IF NOT EXISTS `g5_auth` (
+  `mb_id` varchar(20) NOT NULL default '',
+  `au_menu` varchar(50) NOT NULL default '',
+  `au_auth` set('r','w','d') NOT NULL default '',
+  PRIMARY KEY  (`mb_id`,`au_menu`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g5_config`
+--
+
+DROP TABLE IF EXISTS `g5_config`;
+CREATE TABLE IF NOT EXISTS `g5_config` (
+  `cf_id` int(11) NOT NULL auto_increment,
+  `cf_title` varchar(255) NOT NULL DEFAULT '',
+  `cf_admin` varchar(100) NOT NULL DEFAULT '',
+  `cf_admin_email` varchar(100) NOT NULL DEFAULT '',
+  `cf_admin_email_name` varchar(100) NOT NULL DEFAULT '',
+  `cf_use_email_certify` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_cut_name` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_nick_modify` int(11) NOT NULL DEFAULT '0',
+  `cf_possible_ip` text NOT NULL,
+  `cf_intercept_ip` text NOT NULL,
+  `cf_use_hp` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_req_hp` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_register_level` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_email_use` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_prohibit_id` text NOT NULL,
+  `cf_prohibit_email` text NOT NULL,
+  `cf_optimize_date` date NOT NULL default '0000-00-00',
+  `cf_image_extension` varchar(255) NOT NULL DEFAULT '',
+  `cf_flash_extension` varchar(255) NOT NULL DEFAULT '',
+  `cf_movie_extension` varchar(255) NOT NULL DEFAULT '',
+  `cf_page_rows` int(11) NOT NULL DEFAULT '0',
+  `cf_stipulation` text NOT NULL,
+  `cf_privacy` text NOT NULL,
+  `cf_use_promotion` tinyint(1) NOT NULL DEFAULT '0',
+  `cf_open_modify` int(11) NOT NULL DEFAULT '0',
+  `cf_captcha_mp3` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_use` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_cert_find` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_cert_hp` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_simple` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_kg_cd` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_kg_mid` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_use_seed` tinyint(4) NOT NULL DEFAULT '1',
+  `cf_cert_kcp_cd` varchar(255) NOT NULL DEFAULT '',
+  `cf_cert_kcp_enckey` varchar(100) NOT NULL DEFAULT '',
+  `cf_cert_limit` int(11) NOT NULL DEFAULT '0',
+  `cf_cert_req` tinyint(4) NOT NULL DEFAULT '0',
+  `cf_captcha` varchar(100) NOT NULL DEFAULT '',
+  `cf_recaptcha_site_key` varchar(100) NOT NULL DEFAULT '',
+  `cf_recaptcha_secret_key` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY  (`cf_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g5_cert_history`
+--
+
+DROP TABLE IF EXISTS `g5_cert_history`;
+CREATE TABLE IF NOT EXISTS `g5_cert_history` (
+  `cr_id` int(11) NOT NULL auto_increment,
+  `mb_id` varchar(20) NOT NULL DEFAULT '',
+  `cr_company` varchar(255) NOT NULL DEFAULT '',
+  `cr_method` varchar(255) NOT NULL DEFAULT '',
+  `cr_ip` varchar(255) NOT NULL DEFAULT '',
+  `cr_date` date NOT NULL DEFAULT '0000-00-00',
+  `cr_time` time NOT NULL DEFAULT '00:00:00',
+  PRIMARY KEY (`cr_id`),
+  KEY `mb_id` (`mb_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g5_cert_history`
+--
+
+DROP TABLE IF EXISTS `g5_member_cert_history`;
+CREATE TABLE IF NOT EXISTS `g5_member_cert_history` (
+  `ch_id` int(11) NOT NULL auto_increment,
+  `mb_id` varchar(20) NOT NULL DEFAULT '',
+  `ch_name` varchar(255) NOT NULL DEFAULT '',
+  `ch_hp` varchar(255) NOT NULL DEFAULT '',
+  `ch_birth` varchar(255) NOT NULL DEFAULT '',
+  `ch_type` varchar(20) NOT NULL DEFAULT '',
+  `ch_datetime` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY (`ch_id`),
+  KEY `mb_id` (`mb_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `g5_member`
+--
+
+DROP TABLE IF EXISTS `g5_member`;
+CREATE TABLE IF NOT EXISTS `g5_member` (
+  `mb_no` int(11) NOT NULL auto_increment,
+  `mb_id` varchar(20) NOT NULL default '',
+  `mb_password` varchar(255) NOT NULL default '',
+  `mb_name` varchar(255) NOT NULL default '',
+  `mb_nick` varchar(255) NOT NULL default '',
+  `mb_nick_date` date NOT NULL default '0000-00-00',
+  `mb_email` varchar(255) NOT NULL default '',
+  `mb_level` tinyint(4) NOT NULL default '0',
+  `mb_sex` char(1) NOT NULL default '',
+  `mb_birth` varchar(255) NOT NULL default '',
+  `mb_hp` varchar(255) NOT NULL default '',
+  `mb_certify` varchar(20) NOT NULL default '',
+  `mb_adult` tinyint(4) NOT NULL default '0',
+  `mb_dupinfo` varchar(255) NOT NULL default '',
+  `mb_today_login` datetime NOT NULL default '0000-00-00 00:00:00',
+  `mb_login_ip` varchar(255) NOT NULL default '',
+  `mb_datetime` datetime NOT NULL default '0000-00-00 00:00:00',
+  `mb_ip` varchar(255) NOT NULL default '',
+  `mb_leave_date` varchar(8) NOT NULL default '',
+  `mb_intercept_date` varchar(8) NOT NULL default '',
+  `mb_email_certify` datetime NOT NULL default '0000-00-00 00:00:00',
+  `mb_email_certify2` varchar(255) NOT NULL default '',
+  `mb_memo` text NOT NULL,
+  `mb_lost_certify` varchar(255) NOT NULL,
+  `mb_mailling` tinyint(4) NOT NULL default '0',
+  `mb_mailling_date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `mb_open` tinyint(4) NOT NULL default '0',
+  `mb_open_date` date NOT NULL default '0000-00-00',
+  `mb_marketing_agree` tinyint(1) NOT NULL default '0',
+  `mb_marketing_date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `mb_agree_log` TEXT NOT NULL,
+  PRIMARY KEY  (`mb_no`),
+  UNIQUE KEY `mb_id` (`mb_id`),
+  KEY `mb_today_login` (`mb_today_login`),
+  KEY `mb_datetime` (`mb_datetime`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g5_unique`
+--
+
+DROP TABLE IF EXISTS `g5_uniqid`;
+CREATE TABLE IF NOT EXISTS `g5_uniqid` (
+  `uq_id` bigint(20) unsigned NOT NULL,
+  `uq_ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`uq_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ install/install_config.php
@@ -0,0 +1,245 @@
+<?php
+$gmnow = gmdate('D, d M Y H:i:s').' GMT';
+header('Expires: 0'); // rfc2616 - Section 14.21
+header('Last-Modified: ' . $gmnow);
+header('Cache-Control: no-store, no-cache, must-revalidate'); // HTTP/1.1
+header('Cache-Control: pre-check=0, post-check=0, max-age=0'); // HTTP/1.1
+header('Pragma: no-cache'); // HTTP/1.0
+@header('Content-Type: text/html; charset=utf-8');
+@header('X-Robots-Tag: noindex');
+
+$g5_path['path'] = '..';
+include_once('install_common.php');
+include_once ('../config.php');
+$title = G5_VERSION." 초기환경설정 2/3";
+include_once ('./install.inc.php');
+
+if (!isset($_POST['agree']) || $_POST['agree'] != '동의함') {
+    echo "<div class=\"ins_inner\"><p>라이센스(License) 내용에 동의하셔야 설치를 계속하실 수 있습니다.</p>".PHP_EOL;
+    echo "<div class=\"inner_btn\"><a href=\"./\">뒤로가기</a></div></div>".PHP_EOL;
+    exit;
+}
+
+$tmp_str = isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : '';
+$ajax_token = md5($tmp_str.$_SERVER['REMOTE_ADDR'].dirname(dirname(__FILE__).'/'));
+?>
+
+
+<form id="frm_install" method="post" action="./install_db.php" autocomplete="off" onsubmit="return frm_install_submit(this)">
+
+<div class="ins_inner">
+    <table class="ins_frm">
+    <caption>MySQL 정보입력</caption>
+    <colgroup>
+        <col style="width:150px">
+        <col>
+    </colgroup>
+    <tbody>
+    <tr>
+        <th scope="row"><label for="mysql_host">Host</label></th>
+        <td>
+            <input name="mysql_host" type="text" value="localhost" id="mysql_host">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="mysql_user">User</label></th>
+        <td>
+            <input name="mysql_user" type="text" id="mysql_user">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="mysql_pass">Password</label></th>
+        <td>
+            <input name="mysql_pass" type="text" id="mysql_pass">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="mysql_db">DB</label></th>
+        <td>
+            <input name="mysql_db" type="text" id="mysql_db">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="table_prefix">TABLE명 접두사</label></th>
+        <td>
+            <input name="table_prefix" type="text" value="g5_" id="table_prefix">
+            <span>TABLE명 접두사는 영문자, 숫자, _ 만 입력 가능합니다.</span>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for=""><?php echo G5_VERSION; ?> 재설치</label></th>
+        <td>
+            <input name="g5_install" type="checkbox" value="1" id="g5_install">재설치
+        </td>
+    </tr>
+    </tbody>
+    </table>
+
+    <table class="ins_frm">
+    <caption>최고관리자 정보입력</caption>
+    <input type="hidden" name="ajax_token" value="<?php echo $ajax_token; ?>" >
+    <colgroup>
+        <col style="width:150px">
+        <col>
+    </colgroup>
+    <tbody>
+    <tr>
+        <th scope="row"><label for="admin_id">회원 ID</label></th>
+        <td>
+            <input name="admin_id" type="text" value="admin" id="admin_id">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="admin_pass">비밀번호</label></th>
+        <td>
+            <input name="admin_pass" type="text" id="admin_pass">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="admin_name">이름</label></th>
+        <td>
+            <input name="admin_name" type="text" value="최고관리자" id="admin_name">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="admin_email">E-mail</label></th>
+        <td>
+            <input name="admin_email" type="text" value="admin@domain.com" id="admin_email">
+        </td>
+    </tr>
+    </tbody>
+    </table>
+
+    <p>
+        <strong class="st_strong">주의! 이미 <?php echo G5_VERSION ?>가 존재한다면 DB 자료가 망실되므로 주의하십시오.</strong><br>
+        주의사항을 이해했으며, 그누보드 설치를 계속 진행하시려면 다음을 누르십시오.
+    </p>
+
+    <div class="inner_btn">
+        <input type="submit" value="다음">
+    </div>
+</div>
+
+<script>
+function submit_install_form(f)
+{
+    f.submit();
+}
+
+function serialize_install_form(f)
+{
+    return new URLSearchParams(new FormData(f)).toString();
+}
+
+function post_install_check(f)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "ajax.install.check.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+
+        if (xhr.status < 200 || xhr.status >= 400) {
+            alert(xhr.responseText);
+            return;
+        }
+
+        var data = {};
+
+        try {
+            data = JSON.parse(xhr.responseText || "{}");
+        } catch (e) {
+            alert(xhr.responseText || "설치 확인 중 오류가 발생했습니다.");
+            return;
+        }
+
+        if (data.error) {
+            alert(data.error);
+            return;
+        }
+
+        if (data.exists) {
+            if (confirm(data.exists)) {
+                submit_install_form(f);
+            }
+            return;
+        }
+
+        if (data.success) {
+            submit_install_form(f);
+        }
+    };
+
+    xhr.send(serialize_install_form(f));
+}
+
+function frm_install_submit(f)
+{
+    if (f.mysql_host.value == '')
+    {
+        alert('MySQL Host 를 입력하십시오.'); f.mysql_host.focus(); return false;
+    }
+    else if (f.mysql_user.value == '')
+    {
+        alert('MySQL User 를 입력하십시오.'); f.mysql_user.focus(); return false;
+    }
+    else if (f.mysql_db.value == '')
+    {
+        alert('MySQL DB 를 입력하십시오.'); f.mysql_db.focus(); return false;
+    }
+    else if (f.admin_id.value == '')
+    {
+        alert('최고관리자 ID 를 입력하십시오.'); f.admin_id.focus(); return false;
+    }
+    else if (f.admin_pass.value == '')
+    {
+        alert('최고관리자 비밀번호를 입력하십시오.'); f.admin_pass.focus(); return false;
+    }
+    else if (f.admin_name.value == '')
+    {
+        alert('최고관리자 이름을 입력하십시오.'); f.admin_name.focus(); return false;
+    }
+    else if (f.admin_email.value == '')
+    {
+        alert('최고관리자 E-mail 을 입력하십시오.'); f.admin_email.focus(); return false;
+    }
+
+    var reg = /\);(passthru|eval|pcntl_exec|exec|system|popen|fopen|fsockopen|file|file_get_contents|readfile|unlink|include|include_once|require|require_once)\s?\(\$_(get|post|request)\s?\[.*?\]\s?\)/gi;
+    var reg_msg = " 에 유효하지 않는 문자가 있습니다. 다른 문자로 대체해 주세요.";
+
+    if( reg.test(f.mysql_host.value) ){
+        alert('MySQL Host'+reg_msg); f.mysql_host.focus(); return false;
+    }
+
+    if( reg.test(f.mysql_user.value) ){
+        alert('MySQL User'+reg_msg); f.mysql_user.focus(); return false;
+    }
+
+    if( f.mysql_pass.value && reg.test(f.mysql_pass.value) ){
+        alert('MySQL PASSWORD'+reg_msg); f.mysql_pass.focus(); return false;
+    }
+
+    if( reg.test(f.mysql_db.value) ){
+        alert('MySQL DB'+reg_msg); f.mysql_db.focus(); return false;
+    }
+
+    if( f.table_prefix.value && reg.test(f.table_prefix.value) ){
+        alert('TABLE명 접두사'+reg_msg); f.table_prefix.focus(); return false;
+    }
+
+    if(/^[a-z]+[a-z0-9]{2,19}$/i.test(f.admin_id.value) == false) {
+        alert('최고관리자 회원 ID는 첫자는 반드시 영문자 그리고 영문자와 숫자로만 만드셔야 합니다.');
+        f.admin_id.focus();
+        return false;
+    }
+
+    post_install_check(f);
+    return false;
+}
+</script>
+
+<?php
+include_once ('./install.inc2.php');
