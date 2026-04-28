04c5e5627891033ca4e38033dec2c32a78ecce84
whitedot <whitedot@naver.com>
2026-04-28 17:13:54 +0900

    수정: 회원관리파일 삭제 메뉴 활성화

--- adm/member_list_file_delete.php
+++ adm/member_list_file_delete.php
@@ -2,7 +2,7 @@
 // 검증 지도: 관리자 회원 export 임시 파일 정리 화면 controller다.
 // 정리 실행과 결과 view는 export-maintenance.lib.php에서 담당한다.
 // 이 파일에는 파일 삭제 조건이나 경로 계산 로직을 직접 추가하지 않는다.
-$sub_menu = '100930';
+$sub_menu = '200400';
 require_once './_common.php';
 
 $page_view = admin_complete_member_list_file_delete_request($is_admin);
